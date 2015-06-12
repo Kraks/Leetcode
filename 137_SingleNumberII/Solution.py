165b23178ddc9c04fc10cb1bb0b10f9799e73d65
Kraks <kiss.kraks@gmail.com>
2015-06-13 01:07:47 +0800

    another solution of bit manipulation

--- 137_SingleNumberII/Solution.py
+++ 137_SingleNumberII/Solution.py
@@ -10,3 +10,16 @@ class Solution:
                 count[x] = 1
         for k, v in count.iteritems():
             if v == 1: return k
+
+    def singleNumber1(self, nums):
+        one, two, three = 0, 0, 0
+        for i in range(0, len(nums)):
+            two |= one & nums[i]
+            one ^= nums[i]
+            three = one & two
+            one &= ~three
+            two &= ~three
+        return one
+
+s = Solution()
+print s.singleNumber1([1, 2, 3, 1, 2, 3, 4, 1, 2, 3])
