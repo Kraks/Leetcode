247f294afa53ffbbaa68b3bd6333162ed0a401e9
Kraks <kiss.kraks@gmail.com>
2015-06-23 15:42:05 +0800

    fix

--- 96_UniqueBinarySearchTree/Solution.java
+++ 96_UniqueBinarySearchTree/Solution.java
@@ -7,7 +7,7 @@ public class Solution {
         ways[1] = 1;
         for(int i = 2 ; i <= n; ++i) {
             ways[i] = 0;
-            for(int left = 0; left < i; ++ left) {
+            for(int left = 0; left < i; ++left) {
                 //with number of left noeds in the left sub-tree
                 int lways = ways[left];
                 int rways = ways[i - left - 1];
