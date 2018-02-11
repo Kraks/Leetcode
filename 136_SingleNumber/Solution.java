cb45c2fe146cc7e41ea7f991849cdea4e676449e
Guannan Wei <kiss.kraks@gmail.com>
2018-02-11 16:15:50 -0500

    217, 350, 136

--- 136_SingleNumber/Solution.java
+++ 136_SingleNumber/Solution.java
@@ -6,32 +6,21 @@ import java.util.Map;
  *  * Created by kraks on 3/29/14.
  *   */
 public class Solution {
-    public int singleNumber(int[] A) {
-        Integer key = null;
-        HashMap<Integer, Integer> freq1 = new HashMap<Integer, Integer>();
-        HashMap<Integer, Integer> freq2 = new HashMap<Integer, Integer>();
-        for (int i = 0; i < A.length; i++) {
-            if (!freq1.containsKey(A[i])) {
-                freq1.put(A[i], 1);
-            }
-            else {
-                freq2.put(A[i], 2);
-                freq1.remove(A[i]);
-            }
-        }
-
-        Iterator iter = freq1.entrySet().iterator();
-        while (iter.hasNext()) {
-            Map.Entry entry = (Map.Entry) iter.next();
-            Object k = entry.getKey();
-            key = (Integer)k;
-        }
-        return key;
+  public int singleNumber(int[] nums) {
+    HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
+    for (int i = 0; i < nums.length; i++) {
+      if (map.containsKey(nums[i])) { map.put(nums[i], 2); }
+      else { map.put(nums[i], 1); }
     }
-
-    public static void main(String[] args) {
-        int[] a = {1, 2, 3, 4, 5, 6, 1, 2, 3, 5, 6};
-        Solution solution = new Solution();
-        System.out.println(solution.singleNumber(a));
+    for(Integer key : map.keySet()) {
+      if (map.get(key) == 1) return key;
     }
+    return -1;
+  }
+
+  public static void main(String[] args) {
+    int[] a = {1, 2, 3, 4, 5, 6, 1, 2, 3, 5, 6};
+    Solution solution = new Solution();
+    System.out.println(solution.singleNumber(a));
+  }
 }
